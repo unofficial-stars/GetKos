7f3bad8b949a865646253afafa3e966ef0d9d882
djinggoo <themalekmalek@gmail.com>
2020-11-28 01:29:03 +0700

    add validation, change ui, optimize code and add data dummy

--- app/src/main/java/com/djinggoo/getkos/PredictionActivity.java
+++ app/src/main/java/com/djinggoo/getkos/PredictionActivity.java
@@ -1,84 +1,137 @@
 package com.djinggoo.getkos;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.textfield.TextInputLayout;
+import com.djinggoo.getkos.data.Dummy;
+import com.djinggoo.getkos.utils.TFLite;
+import com.google.android.material.textfield.TextInputEditText;
 
-public class PredictionActivity extends AppCompatActivity {
+import java.util.Arrays;
+import java.util.List;
+
+public class PredictionActivity extends AppCompatActivity implements DialogFacilities.DialogFacilitiesListener {
 
     private AutoCompleteTextView dropdownCityItems;
     private AutoCompleteTextView dropdownAreaItems;
-    private AutoCompleteTextView dropdownBFItems;
-    private AutoCompleteTextView dropdownSFItems;
-    private AutoCompleteTextView dropdownFacilitiesItems;
+    private AutoCompleteTextView dropdownTypeItems;
+
+    private TextInputEditText resultTextInput;
+    private TextView textFacilities;
+
+    private Boolean isBathroomSelected = false;
+    private Boolean isWifiSelected = false;
+    private Boolean isAccessSelected = false;
+
+    private TFLite tfLite;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prediction);
 
-        dropdownCityItems = findViewById(R.id.dropdown_city_items);
-        dropdownAreaItems = findViewById(R.id.dropdown_area_items);
-        dropdownBFItems = findViewById(R.id.dropdown_bf_items);
-        dropdownFacilitiesItems = findViewById(R.id.dropdown_facilities_items);
-        dropdownSFItems = findViewById(R.id.dropdown_sf_items);
+        Button buttonSelectFacilities = findViewById(R.id.btn_select_facilities);
+        Button buttonCalculate = findViewById(R.id.btn_calculate);
+        dropdownCityItems = (AutoCompleteTextView) findViewById(R.id.dropdown_city_items);
+        dropdownAreaItems = (AutoCompleteTextView) findViewById(R.id.dropdown_area_items);
+        dropdownTypeItems = (AutoCompleteTextView) findViewById(R.id.dropdown_type_items);
+        resultTextInput = findViewById(R.id.textinput_result);
+        textFacilities = findViewById(R.id.text_facilities);
 
-        String[] cityItems = new String[]{
-          "surabaya", "malang", "yogyakarta", "semarang"
-        };
-        String[] facilitiesItems = new String[]{
-                "facilities 1", "facilities 2", "facilities 3", "facilities 4"
-        };
-        String[] bathroomsFacilitiesItems = new String[]{
-                "bathroom fac 1", "bathroom fac 2", "bathroom fac 3", "bathroom fac 4"
-        };
-        String[] areaItems = new String[]{
-                "area 1", "area 2", "area 3", "area 4"
-        };
-        String[] sharedFacilitiesItems = new String[]{
-                "sf 1", "sf 2", "sf 3", "sf 4"
-        };
+        buttonSelectFacilities.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialogFacilities();
+
+            }
+        });
+
+        buttonCalculate.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                List<String> cities = Arrays.asList(Dummy.getCityItems());
+                Integer cityValue = cities.indexOf(dropdownCityItems.getText().toString())+1;
+
+                List<String> areas = Arrays.asList(Dummy.getAreaItems());
+                Integer areaValue = areas.indexOf(dropdownAreaItems.getText().toString())+1;
+
+                List<String> types = Arrays.asList(Dummy.getTypeItems());
+                Integer typeValue = types.indexOf(dropdownTypeItems.getText().toString())+1;
+
+                Float result = doInference(cityValue, areaValue, typeValue, isBathroomSelected, isWifiSelected, isAccessSelected);
+                resultTextInput.setText(result.toString());
+            }
+        });
 
         ArrayAdapter<String> adapterCityItems = new ArrayAdapter<>(
                 PredictionActivity.this,
                 R.layout.city_items,
-                cityItems
+                Dummy.getCityItems()
         );
 
         ArrayAdapter<String> adapterAreaItems = new ArrayAdapter<>(
                 PredictionActivity.this,
                 R.layout.area_items,
-                areaItems
+                Dummy.getAreaItems()
         );
 
-        ArrayAdapter<String> adapterFacilitesItems = new ArrayAdapter<>(
+        ArrayAdapter<String> adapterTypeItems = new ArrayAdapter<>(
                 PredictionActivity.this,
-                R.layout.facilities_items,
-                facilitiesItems
-        );
-
-        ArrayAdapter<String> adapterBathroomsFacItems = new ArrayAdapter<>(
-                PredictionActivity.this,
-                R.layout.bathroom_facilities_items,
-                bathroomsFacilitiesItems
-        );
-
-        ArrayAdapter<String> adapterSharedFacItems = new ArrayAdapter<>(
-                PredictionActivity.this,
-                R.layout.shared_facilities_items,
-                sharedFacilitiesItems
+                R.layout.type_kos_items,
+                Dummy.getTypeItems()
         );
 
         dropdownCityItems.setAdapter(adapterCityItems);
         dropdownAreaItems.setAdapter(adapterAreaItems);
-        dropdownFacilitiesItems.setAdapter(adapterFacilitesItems);
-        dropdownBFItems.setAdapter(adapterBathroomsFacItems);
-        dropdownSFItems.setAdapter(adapterSharedFacItems);
+        dropdownTypeItems.setAdapter(adapterTypeItems);
 
+        tfLite = new TFLite(getAssets());
     }
+
+    public void openDialogFacilities(){
+        DialogFacilities dialogFacilities = new DialogFacilities();
+        dialogFacilities.show(getSupportFragmentManager(), "Dialog Facilities");
+    }
+
+    @Override
+    public void applySelection(Float bathroomVal, Float wifiVal, Float accessVal) {
+        String facilities = "Fasilitas : ";
+        if (bathroomVal != 0) facilities += "Kamar Mandi Dalam, ";
+        if (wifiVal != 0) facilities += "Wifi, ";
+        if (accessVal != 0) facilities += "Akses 24 Jam ";
+
+        textFacilities.setText(facilities);
+    }
+
+    private Float doInference(Integer cityValue, Integer areaValue, Integer typeValue, Boolean isBathroomSelected, Boolean isWifiSelected, Boolean isAccessSelected){
+        if (cityValue == 0){
+            Toast.makeText(getApplicationContext(), "Pilih Kota Yang Sesuai!", Toast.LENGTH_SHORT).show();
+            return 0f;
+        }
+        if (areaValue == 0) {
+            Toast.makeText(getApplicationContext(), "Pilih Area Yang Sesuai!", Toast.LENGTH_SHORT).show();
+            return 0f;
+        }
+        if (typeValue == 0) {
+            Toast.makeText(getApplicationContext(), "Pilih Tipe Kos Yang Sesuai", Toast.LENGTH_SHORT).show();
+            return 0f;
+        }
+
+        Float city = Float.valueOf(cityValue);
+        Float area = Float.valueOf(areaValue);
+        Float type = Float.valueOf(typeValue);
+        Float facilities = (float) ((isBathroomSelected ? 2 : 0) + (isWifiSelected ? 2 : 0) + (isAccessSelected ? 2 : 0));
+
+        return tfLite.doInference(city, area, type, facilities);
+    }
+
 }
---
+++ app/src/main/java/com/djinggoo/getkos/data/Dummy.java
@@ -0,0 +1,34 @@
+package com.djinggoo.getkos.data;
+
+public class Dummy {
+
+    public static String[] getAreaItems(){
+     return new String[]{
+                "arcamanik", "babakan ciparay", "bandung kidul", "bandung wetan", "bantul",
+                "banyumanik", "batu", "batununggal", "blimbing", "bojongloa kidul", "buahbatu",
+                "cibeunying kaler", "cicendo", "cidadap", "cilandak", "coblong", "depok",
+                "dukuhpakis", "duren sawit", "gajahmungkur", "gambir", "gayungan", "gedong tengen",
+                "genuk", "godean", "gondokusuman", "gondomanan", "grogol petamburan", "gubeng",
+                "gunung anyar", "jatinangor", "jetis", "karang pilang", "kelapa gading", "kepanjen",
+                "klojen", "koja", "lengkong", "lowokwaru", "mantrijeron", "mergangsan", "mlati",
+                "mulyorejo", "ngaglik", "ngaliyan", "ngemplak", "pakualaman", "palmerah", "pedurungan",
+                "regol", "rungkut", "sawah besar", "sedati", "semarang barat", "semarang selatan",
+                "semarang tengah", "semarang timur", "semarang utara", "senen", "serpong", "setiabudi",
+                "singosari", "sukajadi", "sukolilo", "sukun", "tambaksari", "tanah abang", "tanjung priok",
+                "tebet", "tegalrejo", "tegalsari", "tembalang", "umbulharjo", "waru", "wiyung", "wonocolo", "wonokromo"
+        };
+    }
+
+    public static String[] getTypeItems(){
+        return new String[]{
+                "Campur", "Putra", "Putri"
+        };
+    }
+
+    public static String[] getCityItems(){
+        return new String[]{
+                "Bandung", "Jakarta", "Yogyakarta", "Malang", "Semarang", "Surabaya"
+        };
+    }
+
+}
